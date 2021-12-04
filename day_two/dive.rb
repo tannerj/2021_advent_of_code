e52384a9476b3b94fa54497f5b6afd1fd86cbd8a
Joel Tanner Jones <jonesjoe@ecu.edu>
2021-12-04 15:48:57 -0500

    Solves day two: part two

--- day_two/dive.rb
+++ day_two/dive.rb
@@ -2,13 +2,17 @@
 
 input_file = ARGV[0]
 horizontal_position = 0
+aim = 0
 depth = 0
 
 File.foreach(input_file) do |x|
   command = x.split(" ")
-  horizontal_position += command[1].to_i if command[0] == "forward"
-  depth += command[1].to_i if command[0] == "down"
-  depth -= command[1].to_i if command[0] == "up"
+  if command[0] == "forward"
+    horizontal_position += command[1].to_i
+    depth += (aim * command[1].to_i)
+  end
+  aim += command[1].to_i if command[0] == "down"
+  aim -= command[1].to_i if command[0] == "up"
 end
 
 puts "Horizontal Position x Depth: #{horizontal_position * depth}"
